5b45f4ee626c2c6305c52d60c24196df9f680d13
El Arbi Rikab <elarbi.rikab@etu.univ-lehavre.fr>
2023-12-24 18:34:02 +0100

    Add ValeurFonciere entity class

---
+++ api/src/Entity/ValeurFonciere.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use Doctrine\ORM\Mapping\Column;
+use Doctrine\ORM\Mapping\GeneratedValue;
+use Doctrine\ORM\Mapping\Entity;
+use Doctrine\ORM\Mapping\Id;
+use Symfony\Component\Validator\Constraints\Positive;
+use Symfony\Component\Validator\Constraints\PositiveOrZero;
+
+#[ApiResource]
+#[Entity]
+class ValeurFonciere
+{
+    #[Id]
+    #[GeneratedValue]
+    #[Column(type: 'integer')]
+    protected $identifiant;
+
+    #[Column(type: 'date')]
+    protected $dateMutation;
+
+    #[Column(type: 'integer')]
+    #[Positive]
+    protected $typeMutation;
+
+    #[Column(type: 'float')]
+    #[PositiveOrZero]
+    protected $valeurFonciere;
+
+    #[Column(type: 'integer', length: 3)]
+    #[Positive]
+    protected $codeDepartement;
+
+    #[Column(type: 'integer')]
+    #[Positive]
+    protected $codeTypeLocal;
+
+    public function getIdentifiant(): int
+    {
+        return $this->identifiant;
+    }
+
+    public function getDateMutation(): \DateTimeImmutable
+    {
+        return $this->dateMutation;
+    }
+
+    public function getTypeMutation(): string
+    {
+        return $this->typeMutation;
+    }
+
+    public function getValeurFonciere(): float
+    {
+        return $this->valeurFonciere;
+    }
+
+    public function getCodeDepartement(): int
+    {
+        return $this->codeDepartement;
+    }
+
+    public function getCodeTypeLocal(): int
+    {
+        return $this->codeTypeLocal;
+    }
+}
