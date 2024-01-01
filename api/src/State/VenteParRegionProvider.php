cf1053b1fbe79c08defbc519a8967228387aead5
El Arbi Rikab <elarbi.rikab@etu.univ-lehavre.fr>
2024-01-01 13:57:52 +0100

    fix bug on VenteParRegionProvider

--- api/src/State/VenteParRegionProvider.php
+++ api/src/State/VenteParRegionProvider.php
@@ -21,17 +21,15 @@ class VenteParRegionProvider implements ProviderInterface
 
     public function provide(Operation $operation, array $uriVariables = [], array $context = []): object|array|null
     {
-        $results = $this->ValeurFonciereRepository->findVentesParRegion($uriVariables['annee']);
-        print_r($results);
+        $annee = isset($uriVariables['annee']) ? $uriVariables['annee'] : '2023';
+        $results = $this->ValeurFonciereRepository->findVentesParRegion($annee);
 
         $items = [];
         foreach ($results as $result)
         {
             $salesByRegion = new NombreVenteRegion();
-            // Nettoyer la propriété "region" en supprimant les sauts de ligne
             $result['region'] = str_replace(["\r", "\n"], '', $result['region']);
             $salesByRegion->setRegion($result['region']);
-            
             $salesByRegion->setNombreVente($result['nombreVente']);
             array_push($items, $salesByRegion);
         }
