0ed670911855e47e61b4e944b77515c6b68b0534
aa195113 <abderrahmane.abaakil@etu.univ-lehavre.fr>
2024-01-02 16:08:16 +0100

    Integrate BarChartComponent into Views component

--- pwa/pages/views.tsx
+++ pwa/pages/views.tsx
@@ -1,26 +1,27 @@
 import React from "react";
 import Head from "next/head";
-import LineChartComponent from "./graphs/LineChart"
+import LineChartComponent from "./graphs/LineChart";
+import BarChartComponent from "./graphs/barchart"; // Import BarChartComponent
+
 export default function Views() {
-
-
-    return (
-        <div>
-            <Head>
-                <title>Graphiques</title>
-            </Head>
-            <section className="home-section">
-                <div className="home-content">
-                    <span className="text">Graphiques</span>
-                </div>
-
-                {/* Charts componenets */}
-
-                {/* Line Chart */}
-                <LineChartComponent/>
-                
-
-            </section>
+  return (
+    <div>
+      <Head>
+        <title>Graphiques</title>
+      </Head>
+      <section className="home-section">
+        <div className="home-content">
+          <span className="text">Graphiques</span>
         </div>
-    )
-};
+
+        {/* Charts components */}
+
+        {/* Line Chart */}
+        <LineChartComponent />
+
+        {/* Bar Chart */}
+        <BarChartComponent />
+      </section>
+    </div>
+  );
+}
