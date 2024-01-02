3b955aca51f6317bd7bdfb00d6023a7642b65d8f
Mehdi Khaddor <el-mehdi.khaddor@etu.univ-lehavre.fr>
2024-01-02 15:54:23 +0100

    Set up Views for charts

---
+++ pwa/pages/views.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Head from "next/head";
+import LineChartComponent from "./graphs/LineChart";
+export default function Views() {
+
+
+    return (
+        <div>
+            <Head>
+                <title>Graphiques</title>
+            </Head>
+            <section className="home-section">
+                <div className="home-content">
+                    <span className="text">Graphiques</span>
+                </div>
+
+                {/* Line Chart */}
+                <LineChartComponent/>
+
+            </section>
+        </div>
+    )
+};
