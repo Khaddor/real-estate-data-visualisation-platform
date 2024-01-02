db7fc00b4cad56519b3a60fbc3aa61ff4d236773
Mehdi Khaddor <el-mehdi.khaddor@etu.univ-lehavre.fr>
2024-01-02 16:56:16 +0100

    Set up Views for charts

---
+++ pwa/pages/views.tsx
@@ -0,0 +1,19 @@
+import Head from "next/head";
+
+export default function Views() {
+  return (
+    <div>
+      <Head>
+        <title>Graphiques</title>
+      </Head>
+      <section className="home-section">
+        <div className="home-content">
+          <span className="text">Graphiques</span>
+        </div>
+
+        {/* Charts components */}
+
+      </section>
+    </div>
+  );
+}
