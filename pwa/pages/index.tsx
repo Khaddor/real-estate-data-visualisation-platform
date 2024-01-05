5fd5c3644ca936e59a85b5a49600db719ab4bb47
aa195113 <abderrahmane.abaakil@etu.univ-lehavre.fr>
2024-01-05 17:49:50 +0100

    resize navbar logo

--- pwa/pages/index.tsx
+++ pwa/pages/index.tsx
@@ -33,7 +33,7 @@ export default function Example() {
             >
               <span className="sr-only">Open main menu</span>
               <img
-                className="h-6 w-6" aria-hidden="true"
+                className="h-12 w-12" aria-hidden="true"
                 src="https://cdn.icon-icons.com/icons2/3215/PNG/512/hamburger_menu_navbar_options_icon_196495.png"
                 alt="new"
               />
