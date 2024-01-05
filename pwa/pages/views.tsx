e7950da38bf0ee3a5bf7c401c2829518423a7796
Mehdi Khaddor <el-mehdi.khaddor@etu.univ-lehavre.fr>
2024-01-05 21:09:24 +0100

    Put charts in containers

--- pwa/pages/views.tsx
+++ pwa/pages/views.tsx
@@ -1,9 +1,9 @@
-// Views.js
 import React, { useState } from 'react';
 import NavBar from '../components/NavBar';
 import LineChartComponent from './graphs/LineChart';
 import BarChartComponent from './graphs/BarChart';
 import PieChart from './graphs/PieChart';
+import Footer from "../components/Footer";
 
 const Views = () => {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
@@ -18,12 +18,19 @@ const Views = () => {
       />
 
       <section className="flex justify-center items-center min-h-screen bg-gray-100">
-    {activeChart === 'line-chart' && <LineChartComponent/>}
-    {activeChart === 'bar-chart' && <BarChartComponent/>}
-    {activeChart === 'pie-chart' && <PieChart/>}
-  </section>
-</div>
-);
+        <div
+          className="card bg-base-100 shadow-xl p-6"
+        >
+          <div className="card-body">
+            {/* Place the chart components here */}
+            {activeChart === 'line-chart' && <LineChartComponent />}
+            {activeChart === 'bar-chart' && <BarChartComponent />}
+            {activeChart === 'pie-chart' && <PieChart />}
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default Views;
