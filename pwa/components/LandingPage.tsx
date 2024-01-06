6d4842711f00b6cb09007906418c18b96094e09e
id2ilayss <ilyass.id-abdeslem@etu.univ-lehavre.fr>
2024-01-06 15:42:53 +0100

    edit landing page

--- pwa/components/LandingPage.tsx
+++ pwa/components/LandingPage.tsx
@@ -1,20 +1,10 @@
-import React from 'react';
-import Navbar from './NavBar';
-import Footer from './Footer';
+import React from "react";
+import Navbar from "./NavBar";
+import Footer from "./Footer";
 
 const LandingPage = ({ mobileMenuOpen, setMobileMenuOpen }) => {
   return (
     <div className="bg-white">
-      <div className="flex lg:flex-1">
-          <a href="/" className="m-10 ">
-            <span className="sr-only"></span>
-            <img
-              className="h-8 w-auto"
-              src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-              alt=""
-            />
-          </a>
-        </div>
       <div className="relative isolate px-6 pt-14 lg:px-8">
         <div
           className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
@@ -24,29 +14,20 @@ const LandingPage = ({ mobileMenuOpen, setMobileMenuOpen }) => {
             className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
             style={{
               clipPath:
-                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
             }}
           />
         </div>
         <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
-          <div className="hidden sm:mb-8 sm:flex sm:justify-center">
-            <div
-              className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-              Announcing our next round of funding.{' '}
-              <a href="#" className="font-semibold text-indigo-600">
-                <span className="absolute inset-0" aria-hidden="true"/>
-                Read more <span aria-hidden="true">&rarr;</span>
-              </a>
-            </div>
-          </div>
           <div className="text-center">
             <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
               L'immobilières en France
             </h1>
             <p className="mt-6 text-lg leading-8 text-gray-600">
-              ImmoVista est une application web innovante offrant une expérience immersive dans le monde des ventes
-              immobilières en France. Grâce à des composants graphiques avancés, explorez les données détaillées de
-              manière visuelle et interactive.
+              ImmoVista est une application web innovante offrant une expérience
+              immersive dans le monde des ventes immobilières en France. Grâce à
+              des composants graphiques avancés, explorez les données détaillées
+              de manière visuelle et interactive.
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
               <a
@@ -55,7 +36,10 @@ const LandingPage = ({ mobileMenuOpen, setMobileMenuOpen }) => {
               >
                 Docs
               </a>
-              <a href="/views" className="text-sm font-semibold leading-6 text-gray-900">
+              <a
+                href="/views"
+                className="text-sm font-semibold leading-6 text-gray-900"
+              >
                 Visualiser les données <span aria-hidden="true">→</span>
               </a>
             </div>
@@ -69,12 +53,11 @@ const LandingPage = ({ mobileMenuOpen, setMobileMenuOpen }) => {
             className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
             style={{
               clipPath:
-                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
             }}
           />
         </div>
       </div>
-      <Footer/>
     </div>
   );
 };
