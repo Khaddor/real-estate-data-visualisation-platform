c41c377188c95b9dbab4aa863612cdcc6bf5adfb
id2ilayss <ilyass.id-abdeslem@etu.univ-lehavre.fr>
2024-01-07 20:06:32 +0100

    add NombreVenteRegionTest

---
+++ api/tests/Api/Entity/NombreVenteRegion.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Tests\Api;
+
+use ApiPlatform\Symfony\Bundle\Test\ApiTestCase;
+
+use App\Entity\NombreVenteRegion;
+
+
+class NombreVenteRegionTest extends ApiTestCase
+{
+
+}
