561af64110913f76cc8c8f9901909d7ec21d53a1
id2ilayss <ilyass.id-abdeslem@etu.univ-lehavre.fr>
2024-01-07 19:05:28 +0100

    testGetRequest for ValeurFonciere Entity

--- api/tests/Api/Entity/ValeurFonciereTest.php
+++ api/tests/Api/Entity/ValeurFonciereTest.php
@@ -7,8 +7,25 @@ use ApiPlatform\Symfony\Bundle\Test\ApiTestCase;
 use App\Entity\ValeurFonciere;
 
 
+
 class ValeurFonciereTest extends ApiTestCase
 {
+    public function testGetRequest()
+    {
+        // Create a client and send a GET request to the API
+        $client = static::createClient();
+        $response = $client->request('GET', 'https://localhost/valeur_foncieres?page=1', [
+            'headers' => ['accept' => 'application/ld+json']
+        ]);
+        // Assert that the response is successful
+        $this->assertEquals(200, $response->getStatusCode());
+        // Assert that the response is in JSON
+        $this->assertStringContainsString('application/ld+json', $response->getHeaders()['content-type'][0]);
+        $this->assertStringContainsString('valeurFonciere', $response->getContent());
+        $this->assertStringContainsString('dateMutation', $response->getContent());
+        $this->assertStringContainsString('region', $response->getContent());
+        $this->assertStringContainsString('surface', $response->getContent());
+    }
     
 
 }
