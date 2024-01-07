152c46d93ae8a575cb9f0c5d6476b78279f77d2f
Mehdi Khaddor <el-mehdi.khaddor@etu.univ-lehavre.fr>
2024-01-07 22:48:48 +0100

    Tests for date input

--- pwa/tests/testFront.spec.ts
+++ pwa/tests/testFront.spec.ts
@@ -9,3 +9,9 @@ test('test barchart', async ({ page }) => {
   await page.getByRole('button', { name: 'Bar Chart' }).click();
   await page.locator('#intervalle').selectOption('mois');
 });
+
+test('test barchart date input', async ({ page }) => {
+    await page.goto('https://localhost/views');
+    await page.getByRole('button', { name: 'Bar Chart' }).click();
+    await page.locator('#DateEnd').fill('2019-01-10');
+  });
