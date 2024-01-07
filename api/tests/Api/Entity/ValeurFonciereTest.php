ece0ceb86b3e0b4b1291f0e29bb4c331da1a20ce
id2ilayss <ilyass.id-abdeslem@etu.univ-lehavre.fr>
2024-01-07 18:54:59 +0100

    add ValeurFonciereTest

---
+++ api/tests/Api/Entity/ValeurFonciereTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Tests\Api;
+
+use ApiPlatform\Symfony\Bundle\Test\ApiTestCase;
+
+use App\Entity\ValeurFonciere;
+
+
+class ValeurFonciereTest extends ApiTestCase
+{
+    
+
+}
