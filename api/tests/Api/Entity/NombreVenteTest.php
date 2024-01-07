0bd4cf0c8c37785dc71c9f0f2ff4961c54606ef1
id2ilayss <ilyass.id-abdeslem@etu.univ-lehavre.fr>
2024-01-07 19:58:23 +0100

    test getters and setters for NombreVente entity

--- api/tests/Api/Entity/NombreVenteTest.php
+++ api/tests/Api/Entity/NombreVenteTest.php
@@ -9,4 +9,20 @@ use App\Entity\NombreVente;
 
 class NombreVenteTest extends ApiTestCase
 {
+    public function testGettersAndSetters()
+    {
+        // Create an instance of the NumberSalesByDate class
+        $numberSalesByDate = new NombreVente();
+
+        // Set the date and number of sales using the setters
+        $date = new \DateTime();
+        $numberSales = 123;
+        $numberSalesByDate->setDate($date);
+        $numberSalesByDate->setNombreVente($numberSales);
+
+        // Verify that the getters return the correct values
+        $this->assertEquals($date, $numberSalesByDate->getDate());
+        $this->assertEquals($numberSales, $numberSalesByDate->getNombreVente());
+    }
+
 }
