4d1e81ffdf9d8af918c93469983bb42fab7bb71e
id2ilayss <ilyass.id-abdeslem@etu.univ-lehavre.fr>
2024-01-07 20:09:25 +0100

    test get request for NombreVenteRegion entity

--- api/tests/Api/Entity/NombreVenteRegion.php
+++ api/tests/Api/Entity/NombreVenteRegion.php
@@ -9,5 +9,20 @@ use App\Entity\NombreVenteRegion;
 
 class NombreVenteRegionTest extends ApiTestCase
 {
-
+    public function testGetRequest()
+    {
+        // Create a client and send a GET request to the API
+        $client = static::createClient();
+        // create the get request
+        $response = $client->request('GET', 'https://localhost/ventes_par_region/2022', [
+            'headers' => ['accept' => 'application/json']
+        ]);
+        // Assert that the response is successful
+        $this->assertEquals(200, $response->getStatusCode());
+        // Assert that the response is in JSON
+        $this->assertStringContainsString('application/json', $response->getHeaders()['content-type'][0]);
+        // Assert that the response contains the expected properties
+        $this->assertStringContainsString('region', $response->getContent());
+        $this->assertStringContainsString('nombreVente', $response->getContent());
+    }
 }
