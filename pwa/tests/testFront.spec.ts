815ef28938f38ca2fd42cbbdff29330fc5e6c4b2
Mehdi Khaddor <el-mehdi.khaddor@etu.univ-lehavre.fr>
2024-01-07 22:55:16 +0100

    Tests for line chart

--- pwa/tests/testFront.spec.ts
+++ pwa/tests/testFront.spec.ts
@@ -15,3 +15,9 @@ test('test barchart date input', async ({ page }) => {
     await page.getByRole('button', { name: 'Bar Chart' }).click();
     await page.locator('#DateEnd').fill('2019-01-10');
   });
+
+test('test Line Chart', async ({ page }) => {
+  await page.goto('https://localhost/views');
+  await page.getByRole('button', { name: 'Line Chart' }).click();
+
+});
