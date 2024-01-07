bda79b5235468bdc87d5e7c3e471d5687536d8ef
id2ilayss <ilyass.id-abdeslem@etu.univ-lehavre.fr>
2024-01-07 20:05:00 +0100

    test get request for NombreVente entity

--- api/tests/Api/Entity/NombreVenteTest.php
+++ api/tests/Api/Entity/NombreVenteTest.php
@@ -25,4 +25,22 @@ class NombreVenteTest extends ApiTestCase
         $this->assertEquals($numberSales, $numberSalesByDate->getNombreVente());
     }
 
+
+    public function testGetRequest()
+    {
+        // Create a client and send a GET request to the API
+        $client = static::createClient();
+        // create the get request
+        $response = $client->request('GET', 'https://localhost/nombreVentes/mois/2022-01-01/2022-12-25', [
+            'headers' => ['accept' => 'application/json']
+        ]);
+        // Assert that the response is successful
+        $this->assertEquals(200, $response->getStatusCode());
+        // Assert that the response is in JSON
+        $this->assertStringContainsString('application/json', $response->getHeaders()['content-type'][0]);
+        // Assert that the response contains the expected properties
+        $this->assertStringContainsString('date', $response->getContent());
+        $this->assertStringContainsString('nombreVente', $response->getContent());
+    }
+
 }
